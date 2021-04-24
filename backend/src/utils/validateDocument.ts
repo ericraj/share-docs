ad0b8e9fd577d337c3f9bca5517565b9d09a61dc
Eric Rajaonarison <eric.raj12@gmail.com>
2021-04-24 19:05:55 +0300

    Fix create document resolver

--- backend/src/utils/validateDocument.ts
+++ backend/src/utils/validateDocument.ts
@@ -1,37 +1,27 @@
-import { Category, Tag } from "../entities";
+import { Category } from "../entities";
 import { DocumentInputs, UpdateDocumentInputs } from "../resolvers/documents/inputs";
 import { FieldError } from "../resolvers/error";
 
 export const validateDocument = async (
   inputs: DocumentInputs | UpdateDocumentInputs
 ): Promise<FieldError[] | null> => {
-  if (!inputs.title) {
+  const { title, link, categoryId } = inputs;
+  if (!title) {
     return [{ field: "title", message: "field is required" }];
   }
 
-  if (!inputs.link) {
+  if (!link) {
     return [{ field: "link", message: "field is required" }];
   }
 
-  if (!inputs.categoryId) {
+  if (!categoryId) {
     return [{ field: "categoryId", message: "field is required" }];
   }
 
   // Check category
-  const category = await Category.findOne(inputs.categoryId);
+  const category = await Category.findOne(categoryId);
   if (!category) {
-    return [{ field: "categoryId", message: `category with id ${inputs.categoryId} not found` }];
-  }
-
-  // Check tags
-  if (inputs.tagsIds && inputs.tagsIds.length > 0) {
-    inputs.tagsIds.map(async id => {
-      const tag = await Tag.findOne(id);
-      if (!tag) {
-        return [{ field: "tagsIds", message: `tag with id ${id} not found` }];
-      }
-      return null;
-    });
+    return [{ field: "categoryId", message: `category with id ${categoryId} not found` }];
   }
 
   return null;
